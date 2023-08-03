416f5a971ce7baa7d606a813e61e6ba91a27a02b
VladimirKrivko <mark.tatyschev@gmail.com>
2023-08-03 22:28:25 +0300

    minor change before tests

--- todo-dao/src/main/java/ru/javarush/todolist/query/QuerySql.java
+++ todo-dao/src/main/java/ru/javarush/todolist/query/QuerySql.java
@@ -4,4 +4,8 @@ public class QuerySql {
     public static final String GET_ALL = "SELECT t FROM Task t";
     public static final String GET_ALL_COUNT = "SELECT COUNT(t) FROM Task t";
     public static final String GET_BY_ID = "SELECT t FROM Task t WHERE id=:ID";
+
+    private QuerySql() {
+        throw new UnsupportedOperationException("Utility class");
+    }
 }
